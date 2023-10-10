257f2680b3ef4d9ecaee446d27b40117220a7973
Daniel <114115064+Canu80@users.noreply.github.com>
2023-10-10 19:18:40 -0300

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-
-import '@fortawesome/fontawesome-free/css/all.css';
-import '@fontsource/roboto';
-import '@fontsource-variable/montserrat';
-import App from './App.vue'
-
-createApp(App).mount('#app')
